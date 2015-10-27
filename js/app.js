fc8d7bde35b412b48039a7cffea5cc6c298a6add
Lennyard <allenkevakian@gmail.com>
2015-10-27 09:04:28 -0700

    Add javascript sound

--- js/app.js
+++ js/app.js
@@ -4,16 +4,32 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
-		$(".ryu-still").show();
-		$(".ryu-ready").hide();
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").show();
-		$(".hadouken").show();
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+	
+
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
 	})
 	.mouseup(function() {
-		$(".ryu-throwing").hide();
-		$(".ryu-ready").show();
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
+
 });
+
+function playHadouken () {
+		$('#hadouken-sound')[0].volume=0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+}
